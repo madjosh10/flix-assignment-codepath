0bddd1a8318abe0aa7759215c48a58e7182cbba1
Joshua Madrigal <joshua@joshs.local>
2020-01-29 13:03:15 -0600

    Adding project

---
+++ Flixr/Flixr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixr
+//
+//  Created by Joshua Madrigal on 1/29/20.
+//  Copyright © 2020 Joshua Madrigal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
